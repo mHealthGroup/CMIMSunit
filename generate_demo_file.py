f0e781fdcc6cfa1cd8450f8e28a85967fb35a436
tangqu <tangqu@zepp.com>
2023-01-13 13:33:57 +0800

    Added two small test files

--- generate_demo_file.py
+++ generate_demo_file.py
@@ -9,14 +9,24 @@ total_n = total_time * sr
 
 ts = st
 
-filepath = "data/demo.csv"
+filepath = "data/flat.csv"
 with open(filepath, "w") as f:
     f.write("timestamp,x,y,z\n")
     for i in range(total_n):
-        x = 0.0
-        y = 1.0
-        z = 0.0
+        x = 0
+        y = 1
+        z = 0
         row = f"{ts:.3f},{x:.6f},{y:.6f},{z:.6f}\n"
         ts += 1.0 / sr
         f.write(row)
 
+filepath = "data/random.csv"
+with open(filepath, "w") as f:
+    f.write("timestamp,x,y,z\n")
+    for i in range(total_n):
+        x = random.random()
+        y = random.random()
+        z = random.random()
+        row = f"{ts:.3f},{x:.6f},{y:.6f},{z:.6f}\n"
+        ts += 1.0 / sr
+        f.write(row)
