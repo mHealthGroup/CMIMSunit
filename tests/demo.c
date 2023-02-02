26c5f5fdd38b2581eb34b3a4233d319c212cb281
Aryton Hoi <aryton.a.hoi@gmail.com>
2023-02-02 00:31:51 -0500

    Reset demo to smaller test file

--- tests/demo.c
+++ tests/demo.c
@@ -4,12 +4,12 @@
 int main(int argc, char **argv)
 {
     // Data Set 1
-    // char input_filename[66] = "/Users/arytonhoi/Kode/mhealth/cmims/data/mims_unit/test_2/raw.csv";
-    // char expected_output_filename[71] = "/Users/arytonhoi/Kode/mhealth/cmims/data/mims_unit/test_2/r_output.csv";
+    char input_filename[66] = "/Users/arytonhoi/Kode/mhealth/cmims/data/mims_unit/test_2/raw.csv";
+    char expected_output_filename[71] = "/Users/arytonhoi/Kode/mhealth/cmims/data/mims_unit/test_2/r_output.csv";
 
     // Data Set 2
-    char input_filename[72] = "/Users/arytonhoi/Kode/mhealth/cmims/data/mims_unit/aditya_sleep/raw.csv";
-    char expected_output_filename[77] = "/Users/arytonhoi/Kode/mhealth/cmims/data/mims_unit/aditya_sleep/r_output.csv";
+    // char input_filename[72] = "/Users/arytonhoi/Kode/mhealth/cmims/data/mims_unit/aditya_sleep/raw.csv";
+    // char expected_output_filename[77] = "/Users/arytonhoi/Kode/mhealth/cmims/data/mims_unit/aditya_sleep/r_output.csv";
 
     dataframe_t *mims_data = consistency_test(input_filename);
     precision_test(mims_data, expected_output_filename);
